f13cca7020e2a93eef602924fd18a72bafb6feee
Will Fong <willfong@hey.com>
2021-09-19 23:37:50 +0800

    App

--- index.js
+++ index.js
@@ -4,8 +4,30 @@ import morgan from "morgan";
 const app = express();
 app.use(morgan("combined"));
 
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
 app.get("/", (req, res)=>{
     res.send("Hello World!");
+});
+
+app.get("/sleep", async (req, res)=>{
+    // Not useful for load testing.
+    // It doesn't add any CPU load, just returns slower
+    await sleep(1000)
+    res.send("OK");
+});
+
+app.get("/count", (req, res)=>{
+    // TODO: Find a better way to add CPU load
+    let count = 0;
+    for (let i = 0; i < 20000000; i++) {
+        count += 1;
+    }
+    res.send(`Counted to: ${count}`);
 });
 
 app.get("/error", (req, res)=>{
